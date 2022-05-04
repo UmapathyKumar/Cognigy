b1a6fa47f551b8d30e7869b8c77cc25e462f23d0
UmapathyKumar <kumar.umapathy@gmail.com>
2022-05-04 09:10:57 +0200

    v1added

--- ajax.js
+++ ajax.js
@@ -5,6 +5,7 @@ describe('Ajax_Data', function () {
     it('Ajax Data Test01 ', function () {
         /*To visit the AJAX Data Page*/
         cy.visit("/"+"/ajax")
+        cy.log('tagv1.0')
 
         //Click the button and wait for the data to appear on the label
         cy.get(locators.AjaxRequestButton).click()
